e0e8869a1530a03b3e0d6745ec64f7d87dcae6fa
khoitranminh <86506415+khoitranminh@users.noreply.github.com>
2021-07-01 11:41:29 +0700

    Initial commit

---
+++ WEDBANSACH/Controllers/BookStoreController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEDBANSACH.Models;
+
+namespace WEDBANSACH.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        dbQLBansachDataContext data = new dbQLBansachDataContext();
+        private List<SACH> Laysachmoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+        public ActionResult Index()
+        {
+            var sachmoi = Laysachmoi(5);
+            return View(sachmoi);
+        }
+        public ActionResult Chude()
+        {
+            var chude = from cd in data.CHUDEs select cd;
+            return PartialView(chude);
+        }
+        public ActionResult Nhaxuatban()
+        {
+            var nhaxuatban = from nxb in data.NHAXUATBANs select nxb;
+            return PartialView(nhaxuatban);
+        }
+        public ActionResult SPTheochude(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+        public ActionResult SPTheoNXB(int id)
+        {
+            var sach = from s in data.SACHes where s.MaNXB == id select s;
+            return View(sach);
+        }
+        public ActionResult Chitietsach(int id)
+        {
+            var sach = from s in data.SACHes where s.Masach == id select s;
+            return View(sach.Single());
+        }
+    }
+}
---
+++ WEDBANSACH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WEDBANSACH.MvcApplication" Language="C#" %>
